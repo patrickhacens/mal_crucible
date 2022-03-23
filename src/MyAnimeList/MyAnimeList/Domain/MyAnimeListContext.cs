24b713c1b7081bf82f478e3187aed1bb7fdda07e
Vinicius De Martin <demartinvinicius@gmail.com>
2022-03-23 16:27:56 -0300

    Renomeada pasta Models

--- src/MyAnimeList/MyAnimeList/Domain/MyAnimeListContext.cs
+++ src/MyAnimeList/MyAnimeList/Domain/MyAnimeListContext.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace MyAnimeList.Models;
 
 public class MyAnimeListContext : DbContext
 {
+    public MyAnimeListContext(DbContextOptions<MyAnimeListContext> options) : base(options)
+    {
+
+    }
+    
     public DbSet<Anime> Animes { get; set; }    
     public DbSet<AnimeScore> AnimeScores { get; set; }
     public DbSet<AnimeWithSynopsis> AnimesWithSynopsis { get; set; }
     public DbSet<RatingFromComplete> RatingFromCompletes { get; set; }  
     public DbSet<WatchStatus> WatchStatuses { get; set; }
-
+  
 }
