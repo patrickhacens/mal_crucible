f5400d2c940686371784aeeda3524025399df23f
Vinicius De Martin <demartinvinicius@gmail.com>
2022-03-30 11:32:07 -0300

    Regex inserted to match aired date

--- src/MyAnimeList/MyAnimeList/Features/QtdStudioPeriod/QtdStudioPeriodHandler.cs
+++ src/MyAnimeList/MyAnimeList/Features/QtdStudioPeriod/QtdStudioPeriodHandler.cs
@@ -3,6 +3,7 @@ using Nudes.Retornator.Core;
 using Nudes.Paginator.Core;
 using MyAnimeList.DTO;
 using MyAnimeList.Domain;
+using System.Text.RegularExpressions;
 
 namespace MyAnimeList.Features.QtdStudioPeriod;
 
@@ -16,7 +17,22 @@ public class QtdStudioPeriodHandler : IRequestHandler<QtdStudioPeriodRequest, Re
 
     public Task<ResultOf<PageResult<AnimePerStudioPeriod>>> Handle(QtdStudioPeriodRequest request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+
+        var records = _context.Animes
+            .Where(a => Regex.IsMatch(a.Aired, @"(?i)^\s*(?<month>jan|feb|mar|abr|may|jun|jul|ago|sep|oct|nov|dec)[\.,\s]*\D*(?<day>\d+)[\.,\s]*\D*(?<year>\d{4})"))
+            .Select(a => new
+            {
+                date = Regex.Replace(a.Aired,
+            @"(?i)^\s*(?<month>jan|feb|mar|abr|may|jun|jul|ago|sep|oct|nov|dec)[\.,\s]*\D*(?<day>\d+)[\.,\s]*\D*(?<year>\d{4})",
+            @"Shimoo=${month} ${day}, ${year}")
+            });
+        foreach (var record in records)
+        {
+            Console.WriteLine(record.date);
+        }
+
+
+        return null;
     }
 }
 
