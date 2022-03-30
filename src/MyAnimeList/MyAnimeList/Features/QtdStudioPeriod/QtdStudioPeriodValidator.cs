2a3f0a26b99bb65fd4935edd3d7fdb156777a26f
Vinicius De Martin <demartinvinicius@gmail.com>
2022-03-30 11:32:07 -0300

    Regex inserted to match aired date

--- src/MyAnimeList/MyAnimeList/Features/QtdStudioPeriod/QtdStudioPeriodValidator.cs
+++ src/MyAnimeList/MyAnimeList/Features/QtdStudioPeriod/QtdStudioPeriodValidator.cs
@@ -1,6 +1,11 @@
-﻿namespace MyAnimeList.Features.QtdStudioPeriod
+﻿using FluentValidation;
+
+namespace MyAnimeList.Features.QtdStudioPeriod;
+
+public class QtdStudioPeriodValidator : AbstractValidator<QtdStudioPeriodRequest>
 {
-    public class QtdStudioPeriodValidator
+    public QtdStudioPeriodValidator()
     {
+        RuleFor(p => p).Must(p => (p.ano == 0) ||  (p.mes >= 0 && p.mes <= 12));
     }
 }
