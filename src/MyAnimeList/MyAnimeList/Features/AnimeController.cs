257b1599505bd7cc2bec301c17d8c72754da1817
Vinicius De Martin <demartinvinicius@gmail.com>
2022-03-30 08:53:15 -0300

    Concluído Issue3 - Ranking

--- src/MyAnimeList/MyAnimeList/Features/AnimeController.cs
+++ src/MyAnimeList/MyAnimeList/Features/AnimeController.cs
@@ -1,7 +1,10 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyAnimeList.Domain;
+using MyAnimeList.DTO;
 using MyAnimeList.Features.Import;
+using MyAnimeList.Features.RankingAnime;
 using Nudes.Paginator.Core;
 using Nudes.Retornator.Core;
 
@@ -19,7 +22,12 @@ namespace MyAnimeList
 
         [HttpPost]
         [Route("import")]
-        public Task<ResultOf<bool>>Seed(ImportDataRequest request,CancellationToken cancellation)
+        public Task<ResultOf<bool>>Import(ImportDataRequest request,CancellationToken cancellation)
+            => _mediator.Send(request,cancellation);
+
+        [HttpGet]
+        [Route("ranking")]
+        public Task<ResultOf<PageResult<AnimeRanking>>> Ranking([FromQuery] RankingAnimeRequest request,CancellationToken cancellation)
             => _mediator.Send(request,cancellation);
             
     }
