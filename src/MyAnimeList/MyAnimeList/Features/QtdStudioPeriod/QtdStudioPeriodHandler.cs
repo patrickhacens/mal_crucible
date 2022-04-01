359002d2a14ca4bcd1427241d90a1f4b0c301c22
Vinicius De Martin <demartinvinicius@gmail.com>
2022-04-01 13:26:31 -0300

    Comit para pullrequest

--- src/MyAnimeList/MyAnimeList/Features/QtdStudioPeriod/QtdStudioPeriodHandler.cs
+++ src/MyAnimeList/MyAnimeList/Features/QtdStudioPeriod/QtdStudioPeriodHandler.cs
@@ -1,9 +1,11 @@
 ﻿using MediatR;
 using Nudes.Retornator.Core;
 using Nudes.Paginator.Core;
-using MyAnimeList.DTO;
+using MyAnimeList.Models;
 using MyAnimeList.Domain;
+
 using System.Text.RegularExpressions;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyAnimeList.Features.QtdStudioPeriod;
 
@@ -15,9 +17,33 @@ public class QtdStudioPeriodHandler : IRequestHandler<QtdStudioPeriodRequest, Re
         _context = context;
     }
 
-    public Task<ResultOf<PageResult<AnimePerStudioPeriod>>> Handle(QtdStudioPeriodRequest request, CancellationToken cancellationToken)
+    public async Task<ResultOf<PageResult<AnimePerStudioPeriod>>> Handle(QtdStudioPeriodRequest request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+
+        var records = _context.Studios.Join(_context.Animes, a => a.MyAnimeListId, b => b.MyAnimeListId, (a, b) => new
+        {
+            a.StudioName,
+            b.StartDateAired
+        })
+            .Where( a => !request.ano.HasValue ? true : (a.StartDateAired.HasValue && a.StartDateAired.Value.Year == request.ano) )
+            .Where( a => !request.mes.HasValue ? true : (a.StartDateAired.HasValue && a.StartDateAired.Value.Month == request.mes))
+            .GroupBy(a => a.StudioName).Select(a => new AnimePerStudioPeriod
+        {
+            Studio = a.Key,
+            QtdReleased = a.Count(),
+            anos = a.Min(a => (a.StartDateAired.HasValue ? a.StartDateAired.Value.Year : 9999)).ToString() + " a " + a.Max(a => (a.StartDateAired.HasValue ? a.StartDateAired.Value.Year : 0)).ToString(),
+            meses = a.Min(a => (a.StartDateAired.HasValue ? a.StartDateAired.Value.Month : 12)).ToString() + " a "+a.Max(a => (a.StartDateAired.HasValue ? a.StartDateAired.Value.Month : 0)).ToString()
+        });
+
+        var total = await records.CountAsync(cancellationToken);
+        var items = await records.PaginateByDescending(request, d => d.QtdReleased).ToListAsync(cancellationToken);
+
+
+
+        return new PageResult<AnimePerStudioPeriod>(request, total, items);
+
+
+
     }
 }
 
