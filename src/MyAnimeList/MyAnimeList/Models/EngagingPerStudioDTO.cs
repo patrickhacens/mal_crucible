058a896f1661c8adc475fad1a79bb92c4c962293
Lucas Henrique Russo do Nascimento <lucnascimento98@gmail.com>
2022-04-06 15:34:18 -0300

    Missing file in last commit

---
+++ src/MyAnimeList/MyAnimeList/Models/EngagingPerStudioDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MyAnimeList.Models
+{
+    public class EngagingPerStudioDTO
+    {
+        public string StudioName { get; set; }
+        public double Engaging { get; set; }
+    }
+}
